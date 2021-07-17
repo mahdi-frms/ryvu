118e68a2aeeb1877a7163fe11da204b347cfd646
mahdi-frms <mahdif1380@outlook.com>
2021-07-17 21:50:37 +0430

    add lexer support for > . $

--- crates/ryvu/src/compile.rs
+++ crates/ryvu/src/compile.rs
@@ -1,5 +1,6 @@
 #[derive(PartialEq, Eq, Debug)]
 pub enum TokenKind {
+    Port,
     Identifier,
     Space,
     EndLine,
@@ -70,9 +71,20 @@ impl Lexer {
             if ch == ' ' {
                 self.buffer.push(ch);
             }
-            else if ch == ';' {
+            else if [';','.','>','$'].contains(&ch) {
                 self.push_space();
-                self.tokens.push(token!(Semicolon,";",self.line,self.char_index));
+                if ch == ';' {
+                    self.tokens.push(token!(Semicolon,ch,self.line,self.char_index));
+                }
+                else if ch == '.' {
+                    self.tokens.push(token!(Block,ch,self.line,self.char_index));
+                }
+                else if ch == '$' {
+                    self.tokens.push(token!(Port,ch,self.line,self.char_index));
+                }
+                else {
+                    self.tokens.push(token!(Charge,ch,self.line,self.char_index));
+                }
                 self.char_index += 1;
             }
             else{
@@ -127,17 +139,47 @@ mod test {
         ]);
     }
     #[test]
-    fn supports_semicolons(){
-        let source = "  ; \n   \n;";
+    fn supports_charge(){
+        let source = "  > \n   \n>";
         let tokens = lex(source);
         assert_eq!(tokens,vec![
             token!(Space,"  ",0,0),
-            token!(Semicolon,";",0,2),
+            token!(Charge,">",0,2),
             token!(Space," ",0,3),
             token!(EndLine,"\n",0,4),
             token!(Space,"   ",1,0),
             token!(EndLine,"\n",1,3),
-            token!(Semicolon,";",2,0)
+            token!(Charge,">",2,0)
+        ]);
+    }
+
+    #[test]
+    fn supports_block(){
+        let source = "  . \n   \n.";
+        let tokens = lex(source);
+        assert_eq!(tokens,vec![
+            token!(Space,"  ",0,0),
+            token!(Block,".",0,2),
+            token!(Space," ",0,3),
+            token!(EndLine,"\n",0,4),
+            token!(Space,"   ",1,0),
+            token!(EndLine,"\n",1,3),
+            token!(Block,".",2,0)
+        ]);
+    }
+
+    #[test]
+    fn supports_port(){
+        let source = "  $ \n   \n$";
+        let tokens = lex(source);
+        assert_eq!(tokens,vec![
+            token!(Space,"  ",0,0),
+            token!(Port,"$",0,2),
+            token!(Space," ",0,3),
+            token!(EndLine,"\n",0,4),
+            token!(Space,"   ",1,0),
+            token!(EndLine,"\n",1,3),
+            token!(Port,"$",2,0)
         ]);
     }
 }
