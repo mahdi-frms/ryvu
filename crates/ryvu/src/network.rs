622278c60db523ec0952db79412e8eac4e6050b2
mahdi-frms <mahdif1380@outlook.com>
2021-07-23 13:49:22 +0430

    network: use a single u8 for NodeState

--- crates/ryvu/src/network.rs
+++ crates/ryvu/src/network.rs
@@ -1,17 +1,12 @@
 pub use module::{Module,ModuleBuilder};
 
-#[derive(Default)]
-struct NodeState {
-    charged:bool,
-    blocked:bool,
-    being_charged:bool,
-    being_blocked:bool
-}
-
 pub struct Network {
     module:Module,
     states:Vec<NodeState>
 }
+
+#[derive(Default)]
+struct NodeState (u8);
 
 impl Network {
     pub fn new(module:Module)-> Network {
@@ -26,28 +21,78 @@ impl Network {
     }
 
     pub fn charge(&mut self,index:usize) {
-        self.states[index].charged = true;        
+        self.states[index].set_charged(true);        
     }
     pub fn seek(&self,index:usize) -> bool{
-        self.states[index].charged        
+        self.states[index].get_charged()
     }
     pub fn next(&mut self){
         for index in 0..self.states.len() {
-            if self.states[index].charged && !self.states[index].blocked {
+            if self.states[index].get_charged() && !self.states[index].get_blocked() {
                 for other_index in self.module.connections[index].charging.iter() {
-                    self.states[*other_index].being_charged = true;
+                    self.states[*other_index].set_being_charged(true);
                 }
                 for other_index in self.module.connections[index].blocking.iter() {
-                    self.states[*other_index].being_blocked = true;
+                    self.states[*other_index].set_being_blocked(true);
                 }
             }
         }
         for index in 0..self.module.connections.len() {
-            self.states[index].charged = self.states[index].being_charged;
-            self.states[index].being_charged = false;
-            self.states[index].blocked= self.states[index].being_blocked;
-            self.states[index].being_blocked = false;
+            let charged = self.states[index].get_being_charged();
+            let blocked = self.states[index].get_being_blocked();
+            self.states[index].set_charged(charged);
+            self.states[index].set_being_charged(false);
+            self.states[index].set_blocked(blocked);
+            self.states[index].set_being_blocked(false);
         }
+    }
+}
+
+impl NodeState {
+    fn set_charged(&mut self,value:bool){
+        if value {
+            self.0 |= 0b0001;
+        }
+        else{
+            self.0 &= 0b1110;
+        }
+    }
+    fn set_blocked(&mut self,value:bool){
+        if value {
+            self.0 |= 0b0010;
+        }
+        else{
+            self.0 &= 0b1101;
+        }
+    }
+    fn set_being_charged(&mut self,value:bool){
+        if value {
+            self.0 |= 0b0100;
+        }
+        else{
+            self.0 &= 0b1011;
+        }
+    }
+    fn set_being_blocked(&mut self,value:bool){
+        if value {
+            self.0 |= 0b1000;
+        }
+        else{
+            self.0 &= 0b0111;
+        }
+    }
+
+    fn get_charged(&self)->bool {
+        self.0 % 2 == 0b0001
+    }
+    fn get_blocked(&self)->bool {
+        self.0 % 4 >= 0b0010
+    }
+    fn get_being_charged(&self)->bool {
+        self.0 % 8 >= 0b0100
+    }
+    fn get_being_blocked(&self)->bool {
+        self.0 % 16 >= 0b1000
     }
 }
 
